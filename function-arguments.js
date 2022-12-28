4aef6ce1b7f66579779e46e1b0fa7c84e40c3ade
Maryan <maryanay2000@gmail.com>
2022-12-28 13:45:29 -0500

    first commit

---
+++ function-arguments.js
@@ -0,0 +1,6 @@
+function math (firstNumber, secondNumber, thirdNumber){
+  var value = secondNumber * thirdNumber;
+  value = value + firstNumber
+  return value;
+}
+console.log(math(53, 61, 67));
---
+++ rounding-numbers.js
@@ -0,0 +1,4 @@
+const roundUp = Math.round(1.5)
+const rounded = roundUp
+
+console.log(rounded)
